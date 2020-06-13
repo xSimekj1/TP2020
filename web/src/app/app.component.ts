7c0b4e38ceedb81e7edb92dcac48d62337b0c5fd
Jakub Simek <Jakapo.simek95@gmail.com>
2020-06-13 18:44:23 +0200

    update tsconfig.app.json

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  require: any
   title = 'Team 19 - Rezervacny system';
   data = null;
 
