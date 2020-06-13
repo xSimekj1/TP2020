6364032f84485129dcb7c8cb83e7b17b20282b58
Jakub Simek <Jakapo.simek95@gmail.com>
2020-06-13 15:20:07 +0200

    add basic structure and font awesome

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopmenuComponent } from './components/navigation/topmenu/topmenu.component';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { IntroComponent } from './components/landingpage/intro/intro.component';
+import { TeamComponent } from './components/landingpage/team/team.component';
+import { AboutComponent } from './components/landingpage/about/about.component';
+import { TechnologiesComponent } from './components/landingpage/technologies/technologies.component';
+import { ManagementComponent } from './components/landingpage/management/management.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopmenuComponent
+    TopmenuComponent,
+    LandingpageComponent,
+    IntroComponent,
+    TeamComponent,
+    AboutComponent,
+    TechnologiesComponent,
+    ManagementComponent
   ],
   imports: [
     BrowserModule,
