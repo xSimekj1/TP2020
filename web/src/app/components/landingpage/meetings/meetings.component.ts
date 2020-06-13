05f8346feaaaa7e2569ef6dd4cf69c10d9f0ca8f
Jakub Simek <Jakapo.simek95@gmail.com>
2020-06-13 18:18:29 +0200

    dynamic meeting data loading

--- web/src/app/components/landingpage/meetings/meetings.component.ts
+++ web/src/app/components/landingpage/meetings/meetings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-meetings',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meetings.component.scss']
 })
 export class MeetingsComponent implements OnInit {
+
+  @Input() meetings = null;
 
   constructor() { }
 
